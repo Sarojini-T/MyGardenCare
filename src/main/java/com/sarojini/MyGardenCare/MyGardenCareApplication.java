594eccbad355c904fda413f018f86a462b62e11e
Sarojini Torchon <sarojini.torchon@gmail.com>
2026-01-05 21:10:27 -0500

    Initial Spring Boot project setup

---
+++ src/main/java/com/sarojini/MyGardenCare/MyGardenCareApplication.java
@@ -0,0 +1,13 @@
+package com.sarojini.MyGardenCare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyGardenCareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyGardenCareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sarojini/MyGardenCare/MyGardenCareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sarojini.MyGardenCare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyGardenCareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
