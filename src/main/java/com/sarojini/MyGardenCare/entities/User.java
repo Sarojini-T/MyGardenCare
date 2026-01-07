4af399bfee98e246fe7ab8a26559f37ad87c69c9
Sarojini Torchon <sarojini.torchon@gmail.com>
2026-01-06 22:13:08 -0500

    add user entity

---
+++ src/main/java/com/sarojini/MyGardenCare/entities/User.java
@@ -0,0 +1,65 @@
+package com.sarojini.MyGardenCare.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="users")
+public class User{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column
+    private String city;
+
+    @Column
+    private String state;
+
+//    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+//    private List<Plant> plants;
+
+    public Long getId(){
+        return this.id;
+    }
+
+    public void setId(Long id){
+        this.id = id;
+    }
+
+    public String getUsername(){
+        return this.username;
+    }
+
+    public void setUsername(String username){
+        this.username = username;
+    }
+
+    public String getEmail(){ return this.email;}
+
+    public void setEmail(String email){this.email = email;}
+
+    public String getPassword(){
+        return this.password;
+    }
+
+    public void setPassword(String password){
+        this.password = password;
+    }
+
+    public String getCity(){return this.city; }
+
+    public void setCity(String city){this.city = city;}
+
+    public String getState(){return this.state; }
+
+    public void setState(String state){this.state = state;}
+}
