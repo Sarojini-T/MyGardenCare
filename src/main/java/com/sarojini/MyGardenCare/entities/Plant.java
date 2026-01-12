78c6ee1b7735a4e7cab25f38319388c1576b236a
Sarojini Torchon <sarojini.torchon@gmail.com>
2026-01-12 18:48:36 -0500

    refactor plant entity

--- src/main/java/com/sarojini/MyGardenCare/entities/Plant.java
+++ src/main/java/com/sarojini/MyGardenCare/entities/Plant.java
@@ -1,94 +1,117 @@
 package com.sarojini.MyGardenCare.entities;
 import jakarta.persistence.*;
-import java.time.LocalDate;
 
 @Entity
 @Table(name="plants")
 public class Plant {
-    public enum PlantLocation{
-        WINDOW_SILL,
-        INDOOR_POT,
-        RAISED_BEDS,
-        BALCONY,
-        OUTDOOR_GROUND
-    }
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name="plant_type")
-    private String plantType;
+    @Column(name = "common_name", nullable = false)
+    private String commonName;
 
-    @Column(name="soil_planted_in")
-    private String soilPlantedIn;
+    @Column(name = "scientific_name", nullable =  false, unique = true)
+    private String scientificName;
 
-    @Column(name="date_planted")
-    private LocalDate datePlanted;
+    @Column(name = "water_requirement")
+    private String waterRequirement;
 
-    @Enumerated(EnumType.STRING)
-    @Column(name="planted_location")
-    private PlantLocation plantedLocation;
+    @Column(name = "light_requirement", columnDefinition = "TEXT")
+    private String lightRequirement;
 
-    @Column(name="watering_interval_days")
-    private Integer wateringIntervalDays;
+    @Column(name = "soil_type", columnDefinition = "TEXT")
+    private String soilType;
 
-    @Column(name="is_alive")
-    private Boolean isAlive;
+    @Column(name = "life_cycle", columnDefinition =  "TEXT")
+    private String lifeCycle;
+
+    @Column
+    private Integer height;
+
+    @Column
+    private Integer width;
+
+    @Column
+    private String growth;
 
     public Long getId(){
         return this.id;
     }
 
     public void setId(Long id){
-        this.id = id;
+        this.id =  id;
     }
 
-    public String getPlantType(){
-        return this.plantType;
+    public String getCommonName(){
+        return this.commonName;
     }
 
-    public void setPlantType(String plantType){
-        this.plantType = plantType;
+    public void setCommonName(String commonName){
+        this.commonName = commonName;
     }
 
-    public String getSoilPlantedIn(){
-        return this.soilPlantedIn;
+    public String getScientificName(){
+        return this.scientificName;
     }
 
-    public void setSoilPlantedIn(String soilPlantedIn){
-        this.soilPlantedIn = soilPlantedIn;
+    public void setScientificName(String scientificName){
+        this.scientificName = scientificName;
     }
 
-    public LocalDate getDatedPlanted(){
-        return this.datePlanted;
+    public String getWaterRequirement(){
+        return this.waterRequirement;
     }
 
-    public void setDatePlanted(LocalDate datePlanted){
-        this.datePlanted = datePlanted;
+    public void setWaterRequirement(String waterRequirement){
+        this.waterRequirement = waterRequirement;
     }
 
-    public PlantLocation getPlantedLocation(){
-        return this.plantedLocation;
+    public String getLightRequirement(){
+        return this.lightRequirement;
     }
 
-    public void setPlantedLocation(PlantLocation plantedLocation){
-        this.plantedLocation = plantedLocation;
+    public void setLightRequirement(String lightRequirement){
+        this.lightRequirement += ", " + lightRequirement;
     }
 
-    public Integer getWateringIntervalDays(){
-        return this.wateringIntervalDays;
+    public String getSoilType(){
+        return this.soilType;
     }
 
-    public void setWateringIntervalDays(Integer wateringIntervalDays){
-        this.wateringIntervalDays = wateringIntervalDays;
+    public void setSoilType(String soilType){
+        this.soilType += ", " + soilType;
     }
 
-    public Boolean getIsAlive(){
-        return this.isAlive;
+    public String getLifeCycle(){
+        return this.lifeCycle;
     }
 
-    public void setIsAlive(Boolean isAlive){
-        this.isAlive = isAlive;
+    public void setLifeCycle(String lifeCycle){
+        this.lifeCycle += ", " + lifeCycle;
+    }
+
+    public Integer getHeight(){
+        return this.height;
+    }
+
+    public void setHeight(Integer height){
+        this.height = height;
+    }
+
+    public Integer getWidth(){
+        return this.width;
+    }
+
+    public void setWidth(Integer width){
+        this.width = width;
+    }
+
+    public String getGrowth(){
+        return this.growth;
+    }
+
+    public void setGrowth(String growth){
+        this.growth = growth;
     }
 }
