d5f3b68eea48da9257be70c7224a30ec2d6435be
Sarojini Torchon <sarojini.torchon@gmail.com>
2026-01-20 21:52:09 -0500

    add plant service

---
+++ src/main/java/com/sarojini/MyGardenCare/services/PlantService.java
@@ -0,0 +1,88 @@
+package com.sarojini.MyGardenCare.services;
+
+import com.sarojini.MyGardenCare.dtos.PlantApiDto;
+import com.sarojini.MyGardenCare.dtos.PlantResponse;
+import com.sarojini.MyGardenCare.entities.Plant;
+import com.sarojini.MyGardenCare.repositories.PlantRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PlantService {
+    private final PlantRepository plantRepository;
+
+    public PlantService(PlantRepository plantRepository){
+        this.plantRepository = plantRepository;
+    }
+
+    public PlantResponse getPlantById(Long id){
+        Optional<Plant> plantByIdOptional = plantRepository.findById(id);
+        if(!plantByIdOptional.isPresent()) throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Plant " + id + " not found");
+        return mapPlantToPlantResponse(plantByIdOptional.get());
+    }
+
+    public List<PlantResponse> searchPlantByAnyName(String query){
+        List<Plant> plants = plantRepository.searchByAnyName(query);
+
+        List<PlantResponse> plantResponseList = new ArrayList<>();
+
+        for(Plant plant : plants){
+            plantResponseList.add(mapPlantToPlantResponse(plant));
+        }
+
+        return plantResponseList;
+    }
+
+    public PlantResponse addPlant(PlantApiDto plantApiDto){
+        if(plantRepository.existsByScientificNameIgnoreCase(plantApiDto.getScientificName())){
+            throw new ResponseStatusException(HttpStatus.CONFLICT, plantApiDto.getScientificName() + " already exists");
+        }
+
+        Plant newPlant = mapPlantApiDtoToPlant(plantApiDto);
+        Plant savedPlant = plantRepository.save(newPlant);
+        return mapPlantToPlantResponse(savedPlant);
+    }
+
+    public PlantResponse mapPlantToPlantResponse(Plant plant){
+        PlantResponse newPlantResponse =  new PlantResponse();
+
+        newPlantResponse.setId(plant.getId());
+        newPlantResponse.setCommonName(plant.getCommonName());
+        newPlantResponse.setScientificName(plant.getScientificName());
+
+        if(StringUtils.hasText(plant.getAlternateNames())) newPlantResponse.setAlternateNames(plant.getAlternateNames());;
+        if(StringUtils.hasText(plant.getLightRequirement())) newPlantResponse.setLightRequirement(plant.getLightRequirement());
+        if(StringUtils.hasText(plant.getSoilType())) newPlantResponse.setSoilType(plant.getSoilType());
+        if(StringUtils.hasText(plant.getLifeCycle())) newPlantResponse.setLifeCycle(plant.getLifeCycle());
+        if(StringUtils.hasText(plant.getWaterRequirement())) newPlantResponse.setWaterRequirement(plant.getWaterRequirement());
+        if(plant.getHeightInMeters() != null) newPlantResponse.setHeightInMeters(plant.getHeightInMeters());
+        if(plant.getWidthInMeters() != null) newPlantResponse.setWidthInMeters(plant.getWidthInMeters());
+        if(StringUtils.hasText(plant.getGrowth())) newPlantResponse.setGrowth(plant.getGrowth());
+
+        return newPlantResponse;
+    }
+
+    public Plant mapPlantApiDtoToPlant(PlantApiDto plantApiDto){
+        String commonName = plantApiDto.getCommonName();
+        String scientificName = plantApiDto.getScientificName();
+
+        Plant newPlant = new Plant(commonName, scientificName);
+        newPlant.setAlternateNames(plantApiDto.getAlternateNames());
+        newPlant.setLightRequirement(plantApiDto.getLightRequirement());
+        newPlant.setSoilType(plantApiDto.getSoilType());
+        newPlant.setLifeCycle(plantApiDto.getLifeCycle());
+        newPlant.setWaterRequirement(plantApiDto.getWaterRequirement());
+        newPlant.setHeightInMeters(plantApiDto.getHeightInMeters());
+        newPlant.setWidthInMeters(plantApiDto.getWidthInMeters());
+        newPlant.setGrowth(plantApiDto.getGrowth());
+
+        return newPlant;
+    }
+
+}
