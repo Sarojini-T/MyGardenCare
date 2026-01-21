56dd33d88a1c2119c7af0e3548694c96066bc933
Sarojini Torchon <sarojini.torchon@gmail.com>
2026-01-20 21:52:55 -0500

    add plant response dto

---
+++ src/main/java/com/sarojini/MyGardenCare/dtos/PlantResponse.java
@@ -0,0 +1,18 @@
+package com.sarojini.MyGardenCare.dtos;
+
+import lombok.Data;
+
+@Data
+public class PlantResponse {
+    private Long id;
+    private String commonName;
+    private String scientificName;
+    private String alternateNames;
+    private String lightRequirement;
+    private String soilType;
+    private String lifeCycle;
+    private String waterRequirement;
+    private Double heightInMeters;
+    private Double widthInMeters;
+    private String growth;
+}
